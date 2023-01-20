c04183f7fa159a435ff338b5b5cde28f4d35997f
Tamas K Lengyel <tamas.lengyel@intel.com>
2023-01-20 11:58:09 -0500

    Split out filter.sh

---
+++ query/filter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+REPOSJSON=${1:-"repos.json"}
+
+search() {
+    BUILD=$1
+    jq ".repos[] | select( any(.; .repo.languages.nodes[].name == \"$BUILD\") )" $REPOSJSON | jq -r '.repo.nameWithOwner'
+}
+
+if [ ! -f $REPOSJSON ]; then
+    echo "Please specify input (repos.json)"
+    exit 1
+fi
+
+REPOS=$(search M4)
+REPOS+=$(search CMake)
+REPOS+=$(search Meson)
+
+JSON="["
+for repo in $REPOS; do
+    JSON+="{\"repo\":\"$repo\"},"
+done
+JSON="${JSON%?}"
+JSON+="]"
+
+echo $JSON
